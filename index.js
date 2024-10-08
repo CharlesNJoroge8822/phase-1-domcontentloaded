85134be151a7a0e4858e627ea15e4a39822cc33d
njorogecharles439@gmail.com <njorogecharles439@gmail.com.>
2024-10-08 08:15:05 +0300

    git

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+// document.addEventListener("DOMContentLoaded", function() {
+//     console.log("The DOM has loaded");
+// });
+// document.addEventListener("text", function() {
+//     console.log("This is really cool!");
+// });
+document.addEventListener('DOMContentLoaded', function (){
+    const element = document.getElementById('text');
+    element.textContent = 'This is really cool!'
+
+})
+    
